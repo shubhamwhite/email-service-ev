a929ba5b7f265615800a53fa72d12324d8e98821
Whitelotuscorp <contact@whitelotuscorporation.com>
2025-05-19 17:27:39 +0530

    first commit

---
+++ src/consumers/email.consumer.js
@@ -0,0 +1,26 @@
+const amqp = require('amqplib');
+const sendOtpToEmail = require('../service/emailProvider');
+
+async function startEmailConsumer() {
+  const connection = await amqp.connect('amqp://127.0.0.1');
+  const channel = await connection.createChannel();
+  await channel.assertQueue('emailQueue');
+
+  console.log('Email Service: Waiting for messages...');
+
+  channel.consume('emailQueue', async (msg) => {
+    if (msg !== null) {
+      try {
+        const { email, otp, name, flag } = JSON.parse(msg.content.toString());
+        await sendOtpToEmail(email, otp, name, flag);
+        channel.ack(msg);
+        console.log(`Email sent to: ${email}`);
+      } catch (error) {
+        console.error('Error processing email job:', error.message);
+        channel.nack(msg, false, false); // discard message
+      }
+    }
+  });
+}
+
+module.exports = startEmailConsumer;
